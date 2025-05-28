cf88db906a29739cea48caeaa0f300b9e5db7afa
luiseduardohormiga <luishormiga27@gmail.com>
2025-05-27 23:51:20 -0500

    Proyecto inicial Blackjack

---
+++ Blackjack/src/main/java/com/blackjack/App.java
@@ -0,0 +1,13 @@
+package com.blackjack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
